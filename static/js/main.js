9da46b8b89f87126635f31f0b880f27e41a8dc22
ZCrafter <zach510harrison@gmail.com>
2025-06-08 12:11:12 -0400

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,25 @@
-// Toggle between dark and light mode
-document.getElementById("theme-toggle").addEventListener("click", () => {
-  const body = document.body;
-  if(body.classList.contains("dark-mode")){
-    body.classList.remove("dark-mode");
-    body.classList.add("light-mode");
-  } else {
-    body.classList.remove("light-mode");
-    body.classList.add("dark-mode");
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  // Toggle theme between dark and light mode
+  document.getElementById("theme-toggle").addEventListener("click", () => {
+    const body = document.body;
+    if (body.classList.contains("dark-mode")) {
+      body.classList.remove("dark-mode");
+      body.classList.add("light-mode");
+    } else {
+      body.classList.remove("light-mode");
+      body.classList.add("dark-mode");
+    }
+  });
+
+  // Toggle Side Navigation expansion
+  const navToggle = document.getElementById("nav-toggle");
+  const sideNav = document.getElementById("side-nav");
+  
+  navToggle.addEventListener("click", () => {
+    if (sideNav.classList.contains("expanded")) {
+      sideNav.classList.remove("expanded");
+    } else {
+      sideNav.classList.add("expanded");
+    }
+  });
 });
